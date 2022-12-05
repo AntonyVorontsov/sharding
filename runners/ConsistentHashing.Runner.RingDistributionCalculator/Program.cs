815c2832ccf4c6fe63b5ffa60b9f802bff8fbfc2
Anton Vorontsov <williams@ozon.ru>
2022-12-05 19:29:53 +0300

    Added runners (for proper calculation)

---
+++ runners/ConsistentHashing.Runner.RingDistributionCalculator/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ConsistentHashing;
+using ConsoleTables;
+
+var shards = new[]
+{
+    new Shard("shard-1", numberOfNodes: 1000),
+    new Shard("shard-2", numberOfNodes: 1000),
+    new Shard("shard-3", numberOfNodes: 1000),
+    new Shard("shard-4", numberOfNodes: 1000),
+    new Shard("shard-5", numberOfNodes: 1000)
+};
+var provider = new ConsistentHashingProvider(shards, new XxHashingFunction());
+
+var ring = provider.Ring.VirtualNodesRing;
+var rangeCalculator = shards.ToDictionary(x => x.ShardName, _ => 0L);
+
+var indexOfElement = 0;
+var previousHash = 0;
+foreach (var (hash, virtualNode) in ring)
+{
+    if (indexOfElement == 0)
+    {
+        rangeCalculator[virtualNode.ShardName] += hash - int.MinValue;
+        indexOfElement++;
+        previousHash = hash;
+        continue;
+    }
+
+    if (indexOfElement == ring.Count - 1)
+    {
+        rangeCalculator[virtualNode.ShardName] += int.MaxValue - hash;
+        indexOfElement++;
+        previousHash = hash;
+        continue;
+    }
+
+    rangeCalculator[virtualNode.ShardName] += hash - previousHash;
+    indexOfElement++;
+    previousHash = hash;
+}
+
+const long range = (long)int.MaxValue + int.MaxValue;
+var distributionCalculator = new SortedDictionary<ShardName, float>(
+    rangeCalculator.ToDictionary(
+        x => x.Key,
+        x => (float)x.Value / range * 100));
+
+var averageDistribution = (float)100 / distributionCalculator.Count;
+
+Console.WriteLine("Distribution of ranges on the ring");
+
+Console.WriteLine();
+new ConsoleTable(GetHeaderRow(distributionCalculator.Keys).ToArray())
+    .AddRow(GetDistributionRowValues(distributionCalculator).ToArray())
+    .AddRow(GetDeltaRowValues(distributionCalculator, averageDistribution).ToArray())
+    .Write();
+    
+static IEnumerable<string> GetHeaderRow(IEnumerable<ShardName> shardNames)
+{
+    yield return "Shards:";
+
+    foreach (var shardName in shardNames)
+    {
+        yield return shardName.Value;
+    }
+}
+
+static IEnumerable<object> GetDistributionRowValues(IReadOnlyDictionary<ShardName, float> map)
+{
+    yield return "Distribution %";
+
+    foreach (var (_, value) in map)
+    {
+        yield return value.ToString("n2");
+    }
+}
+
+static IEnumerable<object> GetDeltaRowValues(IReadOnlyDictionary<ShardName, float> map, float averageDistribution)
+{
+    yield return "Delta %";
+
+    foreach (var (_, value) in map)
+    {
+        yield return Math.Abs(value - averageDistribution).ToString("n2");
+    }
+}
---
+++ runners/ConsistentHashing.Runner.ValueDistributionCalculator/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ConsistentHashing;
+using ConsoleTables;
+
+var shards = new[]
+{
+    new Shard("shard-1", numberOfNodes: 1000),
+    new Shard("shard-2", numberOfNodes: 1000),
+    new Shard("shard-3", numberOfNodes: 1000),
+    new Shard("shard-4", numberOfNodes: 1000),
+    new Shard("shard-5", numberOfNodes: 1000)
+};
+var provider = new ConsistentHashingProvider(shards, new XxHashingFunction());
+
+var routingCalculator = shards.ToDictionary(x => x.ShardName, _ => 0);
+
+const int numberOfIdentifiers = 5_000_000;
+foreach (var id in GetIdsRange(numberOfIdentifiers))
+{
+    var shard = provider.Route(id.ToString());
+    routingCalculator[shard]++;
+}
+
+var distributionCalculator = new SortedDictionary<ShardName, float>(
+    routingCalculator.ToDictionary(
+        x => x.Key,
+        x => (float)x.Value / numberOfIdentifiers * 100));
+var averageDistribution = (float)100 / distributionCalculator.Count;
+
+Console.WriteLine("Data distribution per shard");
+Console.WriteLine();
+
+new ConsoleTable(GetHeaderRow(distributionCalculator.Keys).ToArray())
+    .AddRow(GetDistributionRowValues(distributionCalculator).ToArray())
+    .AddRow(GetDeltaRowValues(distributionCalculator, averageDistribution).ToArray())
+    .Write();
+
+static IEnumerable<long> GetIdsRangeNaive(int numberOfIdentifiers)
+{
+    return Enumerable.Range(1, numberOfIdentifiers).Select(x => (long)x);
+}
+
+static IEnumerable<long> GetIdsRange(int numberOfIdentifiers)
+{
+    var random = new Random(Seed: 42);
+
+    var previousValue = 0L;
+    const int constantOffset = 2_500;
+    foreach (var _ in Enumerable.Range(1, numberOfIdentifiers))
+    {
+        var id = previousValue + constantOffset + random.Next(0, constantOffset);
+        yield return id;
+        previousValue = id;
+    }
+}
+
+static IEnumerable<string> GetHeaderRow(IEnumerable<ShardName> shardNames)
+{
+    yield return "Shards:";
+
+    foreach (var shardName in shardNames)
+    {
+        yield return shardName.Value;
+    }
+}
+
+static IEnumerable<object> GetDistributionRowValues(IReadOnlyDictionary<ShardName, float> map)
+{
+    yield return "Distribution %";
+
+    foreach (var (_, value) in map)
+    {
+        yield return value.ToString("n2");
+    }
+}
+
+static IEnumerable<object> GetDeltaRowValues(IReadOnlyDictionary<ShardName, float> map, float averageDistribution)
+{
+    yield return "Delta %";
+
+    foreach (var (_, value) in map)
+    {
+        yield return Math.Abs(value - averageDistribution).ToString("n2");
+    }
+}
